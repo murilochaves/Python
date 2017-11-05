e9d1c64215f12b4e8592c9591445adb3bfbbbbe6
Murilo Chaves <murilochaves@icloud.com>
2017-11-05 19:41:08 -0200

    tentando fazer neuronio mcculloch

---
+++ Inteligência Artificial/Redes Neurais Artificiais/McCulloch-Pitts/mcculloch.py
@@ -0,0 +1,8 @@
+teste = [[0, 0], [0, 1], [1, 0], [1, 1]]
+
+for i in range(len(teste)):
+    a = int(teste[i[0]])
+    print(teste[i][0])
+    print(teste[i][1])
+    #x = 
+    #print(teste[i], '\t',  i)
