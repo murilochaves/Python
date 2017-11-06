3453e8fb8d791600a12273a63486161ca628a8b2
Murilo Chaves <murilochaves@icloud.com>
2017-11-06 03:42:11 -0200

    acho que adicionando neuronio mcculloch

--- Inteligência Artificial/Redes Neurais Artificiais/McCulloch-Pitts/mcculloch.py
+++ Inteligência Artificial/Redes Neurais Artificiais/McCulloch-Pitts/mcculloch.py
@@ -1,12 +1,80 @@
-tabelaAnd = [[0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 1]]
+class McCullochsPitts:
 
-entrada = tabelaAnd
+    def __init__(self, n_conexoes):
+        self.w = [[0]] * n_conexoes
+        self.limiar = 0
+    
+    def set_w(self, w, i):
+        self.w[i] = [w]
 
-print('x1', '\t x2', '\t t', '\t y')
+    def set_limiar(self, limiar):
+        self.limiar = limiar
 
-for i in range(len(entrada)):
-    x1 = entrada[i][0]
-    x2 = entrada[i][1]
-    #t = entrada[i][2]
-    t = x1 and x2
-    print(x1, '\t', x2, '\t', t, '\t')
+# ARRUMAR SOMATORIO - NAO ESTA FUNCIONANDO CORRETAMENTE
+    def soma(self, x):
+        y = 0.0
+        for i in range(len(x)):
+            for j in range(len(x[0])):
+                #print(j)
+                x_neuronio = x[i][j]
+                w_axonio = self.w[j][0]
+                y += x_neuronio * w_axonio
+                #print(x_neuronio, w_axonio, y)
+                #y += x[i][j] * self.w[i]
+        return y
+
+    def ativacao(self, x):
+        s = self.soma(x)
+        if s >= self.limiar:
+            return 1
+        return 0
+
+###########
+## TESTE ##
+###########
+
+# quebrando linha
+print('\n')
+
+# mostrando sobre mcculloch
+print('### Neurônio de McCulloch-Pitts')
+
+# criando neurônio mcculloch-pitts com 2 entradas x1 e x2
+neuronio = McCullochsPitts(2)
+
+# mostrando os valores default
+print('# Default')
+
+# mostrando valores default dos pesos sinapticos
+print ('w\' =', neuronio.w)
+
+# mostrando o valor default do limiar
+print('Limiar\': ', neuronio.limiar)
+
+# mostrando os valores personalizados
+print('\n# Personalizados')
+
+# alterando manualmente os pesos sinapticos
+neuronio.set_w(5, 0)
+neuronio.set_w(5, 1)
+
+# mostrando valores alterados dos pesos sinapticos
+print ('w\'\' =', neuronio.w)
+
+# definindo o limiar de ativação
+neuronio.set_limiar(5)
+
+# mostrando o valor do limiar alterado
+print('Limiar\'\': ', neuronio.limiar)
+
+# definindo a entrada do neurônio
+entrada = [[0, 0], [0, 1], [1, 0], [1, 1]]
+
+# Ativando 
+print('\n# Ativação')
+
+# executando ativação de neurônio
+print(neuronio.ativacao(entrada))
+
+# quebrando linha para encerrar o código
+print('\n')
