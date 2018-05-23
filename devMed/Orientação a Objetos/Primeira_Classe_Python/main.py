a007457fb53f8a7e40c80cceb448c698681a76dc
MuriloChaves <murilochaves@ulbra.edu.br>
2018-05-23 20:36:50 -0300

    teste2

--- devMed/Orientação a Objetos/Primeira_Classe_Python/main.py
+++ devMed/Orientação a Objetos/Primeira_Classe_Python/main.py
@@ -7,7 +7,7 @@ a = PessoaFisica('012.345.678-90', 'Alfredo', 22)
 print('\n### Primeiro Objeto ###')
 print('Nome: ', a.getNome(), ',', a.getIdade(), 'anos, CPF:', a.getCPF())
 
-b = PessoaJuridica('64.164.527/0001-99', 'DevMed', 2)
+b = PessoaJuridica('01.234.567/0001-00', 'DevMed', 2)
 
 print('\n### Segundo Objeto ###')
 print('Empresa: ', b.getNome(), ',', b.getIdade(), 'anos, CNPJ:', b.getCNPJ())
