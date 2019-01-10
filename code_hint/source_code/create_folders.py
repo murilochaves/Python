3cacf32c012b14d3e7099bf2310543033d835aac
Murilo Chaves Jayme <murilochaves@ulbra.edu.br>
2019-01-10 09:05:16 -0200

    Update create_folders.py

--- code_hint/source_code/create_folders.py
+++ code_hint/source_code/create_folders.py
@@ -1,13 +1,14 @@
-import sys
-import os
+#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+# -*- coding: utf-8 -*-
 
-"""
-Este código, possui como objetivo facilitar a criação de uma quantidade de
+"""Este código, possui o objetivo facilitar a criação de uma quantidade de
 pastas tendo um arquivo .txt em branco dentro de cada uma, visando uma
 necessidade do dia-a-dia que tive. A quantidade de pastas é informada via
 console: "python3 ...create_folders.py 12", assim ele criará 12 pastas com o
-.txt desejado.
-"""
+.txt desejado."""
+
+import sys
+import os
 
 
 def execute():
@@ -19,8 +20,10 @@ def execute():
 
     for i in range(folder_quantity):
         os.makedirs('{}/{}./'.format(directory, i+1))
-        file = open('{}/{}./texto.txt'.format(directory, i+1), 'w')
-        file.close()
+        # file = open('{}/{}./texto.txt'.format(directory, i+1), 'w')
+        # file.close()
+        with open('{}/{}./texto.txt'.format(directory, i+1), 'w') as f:
+            pass
 
 if __name__ == "__main__":
     execute()
